2c1a8c3e0d6de85cc3498b358243e9420d79dd4f
ayush-python-code <ayushsinghgautam2004@gmail.com>
2025-08-05 13:08:57 +0530

    Update ProjTxtSpeech.py

--- ProjTxtSpeech.py
+++ ProjTxtSpeech.py
@@ -1,12 +1,11 @@
 import pyttsx3
-# initialize Text-to-speech engine
 print("Welcome to Robot speaker by Ayush singh ")
 print("")
 engine = pyttsx3.init()
 engine.setProperty('rate',150)
-# convert this text to speech
 
 text = input("enter anything which you want me to speak: ")
 engine.say(text)
     
 engine.runAndWait()
+
